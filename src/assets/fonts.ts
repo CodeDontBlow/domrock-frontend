34c333dcfe07bcc44ea3b85edc9f4ca563374ff9
luanaapms <luanamsouza0505@gmail.com>
2026-09-17 10:56:00 -0300

    A6-10-feat: estrutura frontend

---
+++ src/assets/fonts.ts
@@ -0,0 +1,11 @@
+import "@fontsource/inconsolata/300.css";
+import "@fontsource/inconsolata/400.css";
+import "@fontsource/inconsolata/500.css";
+
+import "@fontsource/source-code-pro/400.css";
+
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/500.css";
+
+import "@fontsource/forum/400.css";
+
